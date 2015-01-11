5c965285f91d0d5477eb9a4ce3c2fe59e3d13792
Tedi Konda <tedikonda@gmail.com>
2015-01-11 07:18:23 -0500

    Updated functions exercises instructions.

--- Lessons/Lesson05/Functions exercises.playground/section-1.swift
+++ Lessons/Lesson05/Functions exercises.playground/section-1.swift
@@ -28,6 +28,6 @@
 
 // BONUS TODO: Write a function that accepts a string and returns a boolean indicating whether a string is a palindrome (reads the same backwards or forwards).
 
-// BONUS TODO: Write a function that takes in two strings and returns a boolean indicating whether the two strings
+// BONUS TODO: Write a function that takes in two strings and returns a boolean indicating whether the two strings are equal
 
 // BONUS TODO: Write a function that accepts two parameters, a string and a function that accepts a string and returns a string. Print the result of passing the string into the function 10 times.
