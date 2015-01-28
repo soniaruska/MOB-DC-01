af4832ea16b56324fbab0cf56e4ce55d30a4b82d
soniaruska <smartdesign27@yahoo.com>
2015-01-28 18:36:49 -0500

    adding classwork from last class (programmatically adding elements to IB)

---
+++ Classwork/class 9 - gestures, arrays, tables/Table Views and Scenes/Table Views and Scenes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table Views and Scenes
+//
+//  Created by Sonia Ourmaonva on 1/26/15.
+//  Copyright (c) 2015 Sonia Ourmaonva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
