67532536a1ed72cb2632b4758da77b8def74cf54
Thomas <degry.thomas@gmail.com>
2015-02-02 20:10:24 -0500

    Added Solution for Delegation exercise

---
+++ Lessons/Lesson10/DelegationSolution/Delegation/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  Delegation
+//
+//  Created by Thomas Degry on 29/01/15.
+//  Copyright (c) 2015 Thomas Degry. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    var name:String
+    var location:String
+}
